c6b69bbebc4802bbd2998b0043d7fd13dc3be33e
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-26 22:58:22 -0400

    Finished getting started

---
+++ GettingStarted/OutputtingText/OutputtingText.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	cout << "Starting program..." << flush;
+	cout << "This is a string" << endl;
+	cout << "Banana. " << "Apple. " << "Orange. " << endl;
+	cout << "Here's some more text" << endl;
+
+	return 0;
+}
