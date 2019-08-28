64fae9579ba98bb6bb35d5066b70c53a8629f7a7
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-27 20:20:38 -0400

    Finished Strings, added Input

--- BasicSyntax/Strings/Strings.cpp
+++ BasicSyntax/Strings/Strings.cpp
@@ -1,8 +1,16 @@
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	string text1 = "Hello ";
+	string text2 = "Beef";
+	string text3 = text1 + text2;
+
+	cout << text1 << endl;
+	cout << text2 << endl;
+	cout << text1 << text2 << endl;
+	cout << text3 << endl;
+
 	return 0;
 }
