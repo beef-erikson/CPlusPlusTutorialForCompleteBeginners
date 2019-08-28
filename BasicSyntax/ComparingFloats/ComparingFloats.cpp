97348ab5780d847ddfa253d78d6ae7558cd567e7
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-28 00:36:10 -0400

    Finished ComparingFloats, added ComplexConditions

--- BasicSyntax/ComparingFloats/ComparingFloats.cpp
+++ BasicSyntax/ComparingFloats/ComparingFloats.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
+	float value1 = 1.1;
+	
+	if (value1 == 1.1)
+	{
+		cout << "equal" << endl;
+	}
+	else
+	{
+		cout << "not equal" << endl;
+	}
 
+	cout << setprecision(10) << value1 << endl;
+
+	if (value1 < 1.11)
+	{
+		cout << "equal" << endl;
+	}
+	else
+	{
+		cout << "not equal" << endl;
+	}
 
 	return 0;
 }
