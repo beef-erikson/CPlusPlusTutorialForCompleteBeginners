623d145df20ec7cc785eba854ac3f074559eb4dc
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-28 19:06:44 -0400

    SizeOf completed, SizeOfMultidimensionalArrays added

--- BasicSyntax/SizeOf/SizeOf.cpp
+++ BasicSyntax/SizeOf/SizeOf.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main()
 {
+	int values[] = { 4, 67, 3, 2, 235, 33 };
 
+	for (int i = 0; i < sizeof(values)/sizeof(int); i++)
+	{
+		cout << values[i] << " " << flush;
+	}
 
 	return 0;
 }
