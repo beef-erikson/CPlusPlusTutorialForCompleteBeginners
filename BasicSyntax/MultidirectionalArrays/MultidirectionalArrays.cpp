43e331416c096704ac6b4f797fcae81959451155
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-28 18:50:05 -0400

    Finished MultidirectionalArrays, added SizeOf

--- BasicSyntax/MultidirectionalArrays/MultidirectionalArrays.cpp
+++ BasicSyntax/MultidirectionalArrays/MultidirectionalArrays.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 int main()
 {
+	string animals[][3] =
+	{
+		{"fox", "dog", "cat"},
+		{"mouse", "squirrel", "parrot"}
+	};
 
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << animals[i][j] << " " << flush;
+		}
+
+		cout << endl;
+	}
 
 	return 0;
 }
