434daee30450c87b0c64dbc151676bd6712e21ee
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-27 20:50:36 -0400

    Finished Strings, added IntegerTypes

--- BasicSyntax/Input/Input.cpp
+++ BasicSyntax/Input/Input.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Enter your name: " << flush;
+	string input;
+	cin >> input;
+	cout << "You entered: " << input << endl;
+
+	cout << "Enter a number: " << flush;
+	int value;
+	cin >> value;
+	cout << "You entered: " << value << endl;
 
 	return 0;
 }
