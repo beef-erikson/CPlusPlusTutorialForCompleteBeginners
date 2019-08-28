d21c08d6db9697413411364e1a98a5dc97c3dd15
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-28 17:56:16 -0400

    BreakContinue finished, Arrays added

--- BasicSyntax/BreakContinue/BreakContinue.cpp
+++ BasicSyntax/BreakContinue/BreakContinue.cpp
@@ -3,5 +3,46 @@ using namespace std;
 
 int main()
 {
+	/* Example of break and continue
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "i is: " << i << endl;
+
+		if (i == 1)
+		{
+			continue;
+		}
+
+		if (i == 4)
+		{
+			break;
+		}
+
+		cout << "Looping . . ." << endl;
+	}
+	*/
+
+	const string password = "hello";
+	string input;
+
+	do
+	{
+		cout << "Enter password > " << flush;
+		cin >> input;
+
+		if (input == password)
+		{
+			break;
+		}
+
+		cout << "Incorrect password." << endl;
+
+	} while (true);
+
+	cout << "Password accepted." << endl;
+
+
+	cout << "Program quitting . . ." << endl;
+
 	return 0;
 }
