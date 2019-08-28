757156b57c5e6622a35032f7124bfa0bd1f3a048
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-28 19:14:48 -0400

    SizeOfMultidimensionalArrays finished, Switch added

--- BasicSyntax/SizeOfMultidimensionalArrays/SizeOfMultidimensionalArrays.cpp
+++ BasicSyntax/SizeOfMultidimensionalArrays/SizeOfMultidimensionalArrays.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 int main()
 {
+	string animals[][3] =
+	{
+		{ "fox", "dog", "cat" },
+		{ "mouse", "squirrel", "parrot" }
+	};
 
+	for (int i = 0; i < sizeof(animals) / sizeof(animals[0]); i++)
+	{
+		for (int j = 0; j < sizeof(animals[0]) / sizeof(string); j++)
+		{
+			cout << animals[i][j] << " " << flush;
+		}
+		
+		cout << endl;
+	}
 
 	return 0;
 }
