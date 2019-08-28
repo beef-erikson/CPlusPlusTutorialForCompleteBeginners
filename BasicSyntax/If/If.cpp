ed41eba846fec13963c8cf15e9c2639e02c2df15
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-28 00:00:13 -0400

    Finished If, starting IfElse

--- BasicSyntax/If/If.cpp
+++ BasicSyntax/If/If.cpp
@@ -1,20 +1,24 @@
-// If.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string password = "hello";
+	cout << "Enter password > " << flush;
+	
+	string input;
+	cin >> input;
+
+	if(input == password)
+	{
+		cout << "Correct password." << endl;
+	}
+
+	if (input != password)
+	{
+		cout << "Access denied." << endl;
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
