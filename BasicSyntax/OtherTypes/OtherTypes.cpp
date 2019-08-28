052eccde48dbb78345725ebe703acfeab80f140e
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-27 23:39:20 -0400

    OtherTypes finished, starting If

--- BasicSyntax/OtherTypes/OtherTypes.cpp
+++ BasicSyntax/OtherTypes/OtherTypes.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 int main()
 {
+	bool bValue = true;
+	cout << bValue << endl;
 
+	// checks actual ASCII value
+	char cValue = 'g';
+	cout << (int)cValue << endl;
+	cout << "Size of char: " << sizeof(char) << endl;
+
+	wchar_t wValue = 'i';
+	cout << (char)wValue << endl;
+	cout << "Size of wchar_t: " << sizeof(wchar_t) << endl;
 
 	return 0;
 }
