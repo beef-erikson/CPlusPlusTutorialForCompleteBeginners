efb11a0cd2090b0adf288e9927ca6eecb93cc824
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-28 00:29:25 -0400

    IfElse finished, added ComparingFloats

--- BasicSyntax/IfElse/IfElse.cpp
+++ BasicSyntax/IfElse/IfElse.cpp
@@ -1,20 +1,44 @@
-// IfElse.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
+using namespace std;
+
+int main() 
 {
-    std::cout << "Hello World!\n";
+	cout << "1. \tAdd new record." << endl;
+	cout << "2. \tDelete record." << endl;
+	cout << "3. \tView record." << endl;
+	cout << "4. \tSearch record" << endl;
+	cout << "5. \tQuit" << endl << endl;
+
+	cout << "Enter selection > " << flush;
+
+	int value;
+	cin >> value;
+
+	if (value == 1) 
+	{
+		cout << "Adding new record . . ." << endl;
+	}
+	else if (value == 2) 
+	{
+		cout << "Deleting record . . ." << endl;
+	}
+	else if (value == 3) 
+	{
+		cout << "Viewing record . . ." << endl;
+	}
+	else if (value == 4) 
+	{
+		cout << "Searching record . . ." << endl;
+	}
+	else if (value == 5) 
+	{
+		cout << "Quitting program . . ." << endl;
+	}
+	else 
+	{
+		cout << "Invalid option." << endl;
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
