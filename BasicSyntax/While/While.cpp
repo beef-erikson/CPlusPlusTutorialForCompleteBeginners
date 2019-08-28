47ff80bece7818c655c654229e1afadd34d15c2e
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-28 01:17:21 -0400

    While completed

--- BasicSyntax/While/While.cpp
+++ BasicSyntax/While/While.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 int main()
 {
+	int i = 0;
 
+	while (i < 5)
+	{
+		cout << "Hello " << i << endl;
+		i++;
+	}
+
+	cout << endl << "Program complete." << endl;
 
 	return 0;
 }
