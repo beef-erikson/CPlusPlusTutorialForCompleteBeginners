535874d056339c83e96eba1367a7be64eddfea1a
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-29 08:18:24 -0400

    PrototypesAndIncludes finished, also added utils.h

--- Subroutines/PrototypesAndIncludes/PrototypesAndIncludes.cpp
+++ Subroutines/PrototypesAndIncludes/PrototypesAndIncludes.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include "utils.h"
+
 using namespace std;
 
+// Prototype example - same thing as in utils.h
+//void doSomething();
+
+// Main
 int main()
 {
-
+	doSomething();
+	doSomething();
 
 	return 0;
+}
+
+// Prints Hello
+void doSomething()
+{
+	cout << "Hello" << endl;
 }
