f0797336ab38d2f7a2671f00a2c421134d493c2a
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-29 07:58:21 -0400

    Functions complete, PrototypesAndIncludes added

--- Subroutines/Functions/Functions.cpp
+++ Subroutines/Functions/Functions.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
 using namespace std;
 
+
+// Shows the menu
+void showMenu()
+{
+	cout << "1. Search" << endl;
+	cout << "2. View Record" << endl;
+	cout << "3. Quit" << endl;
+}
+
+// Gets user input
+int getInput()
+{
+	cout << "Enter selection > " << flush;
+
+	int input;
+	cin >> input;
+
+	return input;
+}
+
+// Processes the user input
+void processInput(int input)
+{
+	switch (input)
+	{
+	case 1:
+		cout << "Searching . . ." << endl;
+		break;
+	case 2:
+		cout << "Viewing . . ." << endl;
+		break;
+	case 3:
+		cout << "Quitting . . ." << endl;
+		break;
+	default:
+		cout << "Please select an item from the menu > " << flush;
+	}
+}
+
+// Main
 int main()
 {
-
+	showMenu();
+	int input = getInput();
+	processInput(input);
 
 	return 0;
 }
