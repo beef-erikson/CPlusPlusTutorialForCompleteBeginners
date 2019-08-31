e38c6524f627e55c372a1c309456a9940c483cdc
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-31 15:58:12 -0400

    StringStreams complete, OverloadingConstructors started

--- ObjectOrientatedCoding/StringStreams/StringStreams.cpp
+++ ObjectOrientatedCoding/StringStreams/StringStreams.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <sstream>
+
+using namespace std;
 
 int main()
 {
-    
+	int age = 38;
+	string name = "Beef";
+	
+	stringstream ss;
+
+	ss << "Name is: ";
+	ss << name;
+	ss << "; Age is: ";
+	ss << age;
+
+	string info = ss.str();
+
+	cout << info << endl;
 
 	return 0;
 }
