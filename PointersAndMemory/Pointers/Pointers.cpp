17e7d796c5b6c62ebac0f71b9f1c77a36fa4df3a
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-31 21:46:35 -0400

    Pointers finished, Arithmetic started

--- PointersAndMemory/Pointers/Pointers.cpp
+++ PointersAndMemory/Pointers/Pointers.cpp
@@ -2,9 +2,32 @@
 
 using namespace std;
 
+
+void manipulate(double *pValue)
+{
+	cout << "2. Value of double in manipulate: " << *pValue << endl;
+	*pValue = 10.0;
+	cout << "3. Value of int in manipulate: " << *pValue << endl;
+}
+
+
 int main()
 {
-    
+	int nValue = 8;
+	int *pValue = &nValue;
+	*pValue = 6;
 
+	cout << "Int value: " << nValue << endl;
+	cout << "Pointer to int address: " << pValue << endl;
+	cout << "Int value via pointer: " << *pValue << endl;
+	cout << "--------------------------" << endl;
+
+	double dValue = 123.4;
+
+	cout << "1. dValue: " << dValue << endl;
+	manipulate(&dValue);
+	cout << "4. dValue: " << dValue << endl;
+
+	
 	return 0;
 }
