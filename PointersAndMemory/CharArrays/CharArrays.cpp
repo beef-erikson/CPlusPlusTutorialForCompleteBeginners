ff6432a2f1d9ba25fa3d5c820e7aafbdc50b7d15
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-01 23:41:56 -0400

    CharArrays complete, ReversingString added

--- PointersAndMemory/CharArrays/CharArrays.cpp
+++ PointersAndMemory/CharArrays/CharArrays.cpp
@@ -3,7 +3,33 @@ using namespace std;
 
 int main()
 {
+	char text[] = "hello";
+	
+	// loops through array
+	for (int i = 0; i < sizeof(text); i++)
+	{
+		cout << i << ": " << text[i] << endl;
+	}
 
+	// shows null string terminator
+	for (int i = 0; i < sizeof(text); i++)
+	{
+		cout << i << ": " << (int)text[i] << endl;
+	}
+
+	// breaks loop at null terminator
+	int k = 0;
+	cout << endl;
+	while (true)
+	{
+		if (text[k] == 0)
+		{
+			break;
+		}
+			cout << text[k] << flush;
+			k++;
+	}
+	cout << endl;
 
 	return 0;
 }
