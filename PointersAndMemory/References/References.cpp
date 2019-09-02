82ad6c04ceff5307607a41f5d21ec4c868b89a9d
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-02 02:17:12 -0400

    References completed, Const added

--- PointersAndMemory/References/References.cpp
+++ PointersAndMemory/References/References.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 
+// changes whatever value is passed in
+void changeSomething(double &value)
+{
+	value = 123.4;
+}
+
 int main()
 {
-	
+	// reference example. basically value2 is just a synonym of value1
+	int value1 = 8;
+	int& value2 = value1;
+	value2 = 10;
+
+	cout << "Value 1: " << value1 << "  Value 2: " << value2 << endl;
+
+	// function that takes a reference variable
+	double value = 4.321;
+	changeSomething(value);
+	cout << value << endl;
 
 	return 0;
 }
