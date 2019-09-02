c66c8301bbe8d3ac8d91b2ec76765dbd3c1aa660
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-02 02:40:46 -0400

    Const completed, CopyConstructors added

--- PointersAndMemory/Const/Const.cpp
+++ PointersAndMemory/Const/Const.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 using namespace std;
 
+class Animal
+{
+private:
+	string name;
+
+public:
+	void setName(string name) { this->name = name; };
+	void speak() const { cout << "My name is: " << name << endl; };
+};
+
+
 int main()
 {
+	const double PI = 3.141592;
+	//VALUE = 10;	Cannot change a const.
+	cout << PI << endl;
 
+	Animal animal;
+	animal.setName("Freddy");
+	animal.speak();
+
+	int value = 8;
+	// const int* const pValue = &value;  this will cause both below to error.
+	int* pValue = &value;
+	cout << *pValue << endl;
+
+	int number = 11;
+	pValue = &number;	// error with this: int* const pValue = &value;
+	*pValue = 12;		// error with this: const int* pValue = &value;
+
+	cout << *pValue << endl;
 
 	return 0;
 }
