47edf9a0721759bd57a6afbe18d2c7bc6a8ca4f2
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-03 05:29:58 -0400

    Encapsulation complete, ConstructorInheritance added

--- Inheritance/Encapsulation/Encapsulation.cpp
+++ Inheritance/Encapsulation/Encapsulation.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 using namespace std;
 
+class Frog
+{
+// private instance data
+private:
+	string name;
+
+// private methods
+private:
+	string getName() { return name; }
+
+// constructor
+public:
+	Frog(string name) : name(name) {}
+
+// public methods
+public:
+	void info() { cout << "My name is: " << getName() << endl; }
+};
+
 int main()
 {
-	
+	Frog frog("Freddy");
+	frog.info();
 
 	return 0;
 }
