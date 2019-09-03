15c3f14f026afb016b004b9dac789eb1baf886ef
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-03 05:16:08 -0400

    Inheritance complete, added Encapsulation

--- Inheritance/Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance/Inheritance.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 using namespace std;
 
+class Animal
+{
+public:
+	void speak() { cout << "Grrr" << endl; }
+};
+
+class Cat : public Animal
+{
+public:
+	void jump() { cout << "Cat jumping . . ." << endl; }
+};
+
+class Tiger : public Cat
+{
+public:
+	void attackAntelope() { cout << "Attacking . . ." << endl; }
+};
+
 int main()
 {
+	Animal animal;
+	animal.speak();
 
+	Cat cat;
+	cat.speak();
+	cat.jump();
+
+	Tiger tiger;
+	tiger.speak();
+	tiger.jump();
+	tiger.attackAntelope();
 
 	return 0;
 }
