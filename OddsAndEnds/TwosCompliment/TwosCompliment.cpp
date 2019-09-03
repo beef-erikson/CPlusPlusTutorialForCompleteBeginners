0bee5c7c73e0a3de113d1f30863051e06af274a6
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-03 07:47:41 -0400

    TwosCompliment finished, Static added

--- OddsAndEnds/TwosCompliment/TwosCompliment.cpp
+++ OddsAndEnds/TwosCompliment/TwosCompliment.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main()
 {
+	char value = 127;
 
+	cout << (int)value << endl;
+	// overflows to minimum
+	value++;
+	cout << (int)value << endl;
+
+	// why? - char is 8 bits. 2 pow 8 = 256. 256 / 2 = 128 (for postive and negative).
+	// must store 0, hence 127 max / -128 minimum
 
 	return 0;
 }
