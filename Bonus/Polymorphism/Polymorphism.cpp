db619451b79e2c13b69dff64ff9778130607e2b2
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-05 17:13:42 -0400

    Bonus section added, Polymorphism completed

---
+++ Bonus/Polymorphism/Polymorphism.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+// demonstrating of polymorphism - use virtual when you know the value is going to be overridden in subclass
+class Animal
+{
+public:
+	virtual void speak() = 0;
+};
+
+class Cat : public Animal {
+public:
+	virtual void speak()
+	{
+		cout << "RrrrRRrrrr" << endl;
+	}
+};
+
+class HouseCat : public Cat {
+public:
+	virtual void speak()
+	{
+		cout << "Meouw" << endl;
+	}
+};
+
+
+int main()
+{
+	Cat* pCat = new Cat;
+	pCat->speak();
+	delete pCat;
+
+	HouseCat houseCat;
+	houseCat.speak();
+}
