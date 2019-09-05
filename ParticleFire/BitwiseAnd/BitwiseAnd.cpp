6817393fb30da43dceb838ab655fa276d84f5617
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-04 23:01:19 -0400

    BitwiseAnd added for example to strip colors as well as bit shifting

---
+++ ParticleFire/BitwiseAnd/BitwiseAnd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int color = 0x123456;
+
+	// strips color values using bitwise &
+	unsigned char red = (color & 0xFF0000) >> 16;
+	unsigned char green = (color & 0x00FF00) >> 8;
+	unsigned char blue = color & 0x0000FF;
+
+	cout << hex << (int)red << " " << (int)green << " " << (int)blue << endl;
+
+	// or the following
+	unsigned char red2 = color >> 16;
+	unsigned char green2 = color >> 8;
+	unsigned char blue2 = color;
+
+	cout << hex << (int)red2 << " " << (int)green2 << " " << (int)blue2 << endl;
+
+	return 0;
+}
